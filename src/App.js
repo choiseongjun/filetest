87076c5fe80525a7a90628847f8aebc78779a15b
choiseongjun <csj2702@naver.com>
2024-01-29 14:02:28 +0900

    ,

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { App as SendbirdApp } from "sendbird-uikit";
+import "sendbird-uikit/dist/index.css";
+import { MessageSearch } from "@sendbird/uikit-react";
 
+const APP_ID = "CAE7CC50-CFFC-4F6A-96AD-23F60BA85762";
+const USER_ID = "ordercheck";
 function App() {
   return (
     <div className="App">
-      <div
+      {/* <div
         style={{
           display: "flex",
           justifyContent: "center",
@@ -14,7 +19,8 @@ function App() {
         }}
       >
         <input type="file" accept="image/*" />
-      </div>
+      </div> */}
+      <SendbirdApp appId={APP_ID} userId={USER_ID} />
     </div>
   );
 }
