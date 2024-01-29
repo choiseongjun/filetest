dab460438cdf726e022be26a09f9a63f0eedb6c5
choiseongjun <csj2702@naver.com>
2024-01-29 14:13:49 +0900

    ㅇㄴ

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import "./App.css";
-import { App as SendbirdApp } from "sendbird-uikit";
-import "sendbird-uikit/dist/index.css";
+import React, { useEffect } from "react";
+import SBProvider from "@sendbird/uikit-react/SendbirdProvider";
+import "./style.css";
+import "@sendbird/uikit-react/dist/index.css";
+import MobileView from "./MobileView";
+import DesktopView from "./DesktopView";
 
-const APP_ID = "CAE7CC50-CFFC-4F6A-96AD-23F60BA85762";
-const USER_ID = "ordercheck";
 function App() {
+  const APP_ID = "CAE7CC50-CFFC-4F6A-96AD-23F60BA85762";
+  const USER_ID = "ordercheck";
+
+  const myColorSet = {
+    "--sendbird-light-primary-100": "rgb(195, 241, 169)",
+    "--sendbird-light-primary-300": "rgb(165, 211, 149)",
+    "--sendbird-light-primary-400": "rgb(136, 188, 132)",
+  };
+
+  const [mobileViewOpen, setMobileViewOpen] = React.useState(false);
+  window.addEventListener("resize", function () {
+    if (window.innerWidth < 900) {
+      setMobileViewOpen(true);
+    } else {
+      setMobileViewOpen(false);
+    }
+  });
+
+  useEffect(() => {
+    if (window.innerWidth < 900) {
+      setMobileViewOpen(true);
+    } else {
+      setMobileViewOpen(false);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      {/* <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100vw", 
-          height: "100vh",
-        }}
-      >
-        <input type="file" accept="image/*" />
-      </div> */}
-      {/*sd */}
-      <SendbirdApp appId={APP_ID} userId={USER_ID} />
-    </div>
+    <SBProvider
+      appId={APP_ID}
+      userId={USER_ID}
+      // nickname={NICKNAME}
+      // accessToken={ACCESS_TOKEN}
+      colorSet={myColorSet}
+    >
+      {mobileViewOpen ? <MobileView /> : <DesktopView />}
+    </SBProvider>
   );
 }
 
